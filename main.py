962c1a21141f1f52a266d80dcd15863578cd7f54
DaemoniaMoon <67068723+DaemoniaMoon@users.noreply.github.com>
2020-06-26 10:50:49 +0200

    modification synthaxe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-capEnMax = int(input("Entrez l'énergie maximum dont dispose le train"))
-chargMax = int(input("Entrez la charge maximum que le train peut transporter"))
-pointRecharge = int(input("Entrez le nombre de point de recharge du train"))
-longRail = 
+capEnMax = int(input("Entrez l'énergie maximum dont dispose le train : "))
+chargMax = int(input("Entrez la charge maximum que le train peut transporter : "))
+pointRecharge = int(input("Entrez le nombre de point de recharge du train : "))
+longRail = 100
+
