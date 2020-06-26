58fe3fc6b1381aea359a458fa82a46066c09cfdd
DaemoniaMoon <67068723+DaemoniaMoon@users.noreply.github.com>
2020-06-26 11:35:09 +0200

    update code

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-capEnMax = int(input("Entrez l'énergie maximum dont dispose le train : "))
-chargMax = int(input("Entrez la charge maximum que le train peut transporter : "))
-pointRecharge = int(input("Entrez le nombre de point de recharge du train : "))
-longRail = 100
 
+from   import
+
+<! -- def des fonctions -->
+
+
+
+<! -- def de variable -->
+Energy = int(input("De combien d'énergie dispose le train ? :  "))
+trainDepart = 0
+Container = []
+trainway = []
+serviceStation = []
+pikUp = True
+longWay = int(input("longueur de la voie"))
+maxEnergy = int(input("Energie max"))
+maxCharge = int(input("charge max"))
+statutParty = "ok"
+map = ["-"] * longWay
+consumption = 1
+blocEnergy = int(input("Combien de bloc d'énergie seront disposés ? : "))
+blocEnergyMax = int(input("Combien d'énergie il y a au max ? : "))
+
+
+for i in range(len(blocEnergy)) :
+    serviceStation.append(random.randint(1,10))
+
+<! -- def du main -->
+
+# condition de fin de jeu
+
+while Energy > 0 :
+    continue
+    if maxEnergy = 0 :
+    print("C'est perdu le train est en panne")
+    break 
