e00b361dc4c0b32e3e4cd0d9e4fbeb0f6e1f699f
DaemoniaMoon <67068723+DaemoniaMoon@users.noreply.github.com>
2020-06-26 10:49:10 +0200

    création variables départ

---
+++ main.py
@@ -0,0 +1,4 @@
+capEnMax = int(input("Entrez l'énergie maximum dont dispose le train"))
+chargMax = int(input("Entrez la charge maximum que le train peut transporter"))
+pointRecharge = int(input("Entrez le nombre de point de recharge du train"))
+longRail = 
